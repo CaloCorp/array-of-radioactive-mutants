c80493c4b0483b90137e3a5816081cf59b3cea6b
Calo Corp <marc.calo@codeimmersives.com>
2020-06-29 13:18:23 -0400

    completed

--- main.js
+++ main.js
@@ -3,9 +3,43 @@
  ******************/
 
 
+const addMeToEnd=function(array){
+array.push('Colin')
 
+}
 
+const addMeToStart = function(array){
 
+  array.unshift('Colin')
+}
+
+const changeLast = function(array,value){
+
+  array[array.length-1] = value
+}
+ const changeAllValuesTo = function(array,value){
+
+  for(let i=0; i< array.length;i++)
+  array[i]=value
+ }
+
+const oddOrEven = function(array){
+  for(let i=0; i< array.length;i++)
+  if (array[i] % 2 ===0){
+
+    array[i] = 'even'
+  }
+else array[i]= 'odd'
+
+}
+
+const changeNextThreeToValue = function(start,array,value){
+
+  for(let i=start;i < start + 3;i++){
+    array[i]= value
+  }
+
+}
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
